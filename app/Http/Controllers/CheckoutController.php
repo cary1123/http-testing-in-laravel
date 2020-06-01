505ad016b1b994d9b3de325b0813c97cb4af6737
cary <cary@gentrice.net>
2020-06-01 18:26:53 +0800

    add new test project

---
+++ app/Http/Controllers/CheckoutController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Request;
+
+class CheckoutController extends Controller
+{
+    use RefreshDatabase;
+    public function index(Request $request)
+    {
+
+    }
+}
