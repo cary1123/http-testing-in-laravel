03bf594328c04927bf9f78aa7a71023e72420d6d
cary <cary@gentrice.net>
2020-06-01 18:29:03 +0800

    讀取request data 計算總筆數及總金額。

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -11,6 +11,22 @@ class CheckoutController extends Controller
     use RefreshDatabase;
     public function index(Request $request)
     {
+        $totalAmount = 0;
+        $totalCount = 0;
+         if($request->has('products')){
 
+             foreach ($request->input('products') as $item){
+                 $product = Product::findOrFail($item['id']);
+                 $totalAmount += $product->price * $item['quantity'];
+                 $totalCount += $item['quantity'];
+             }
+         }
+
+         $data = [
+           'totalCount' => $totalCount,
+           'totalAmount' => $totalAmount,
+         ];
+
+         return view('checkout.index',$data);
     }
 }
